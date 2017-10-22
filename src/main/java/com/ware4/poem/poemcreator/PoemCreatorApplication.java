f372113b8514fc47b07291172468e51f24c774df
yanchangyou <yanchangyou@gmail.com>
2017-10-22 22:39:57 +0800

    init

---
+++ src/main/java/com/ware4/poem/poemcreator/PoemCreatorApplication.java
@@ -0,0 +1,12 @@
+package com.ware4.poem.poemcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoemCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoemCreatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ware4/poem/poemcreator/PoemCreatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ware4.poem.poemcreator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoemCreatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
